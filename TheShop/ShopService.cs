240233bcd90bf016a387006ab10c0179649fa598
Mirko Katanic <mirko.katanic@gmail.com>
2019-12-04 20:16:35 +0100

    Number of suppliers is no longer fixed in ShopService, also two separate methods for Ordering and Selling the article are added

--- TheShop/ShopService.cs
+++ TheShop/ShopService.cs
@@ -6,97 +6,82 @@ namespace TheShop
 {
 	public class ShopService
 	{
-		private IDatabaseDriver DatabaseDriver;
+		private IDatabaseDriver databaseDriver;
 		private ILogger logger;
 
-		private ISupplier Supplier1;
-		private ISupplier Supplier2;
-		private ISupplier Supplier3;
+        private ISupplier[] suppliers;
 		
-		public ShopService(IDatabaseDriver databaseDriver, ILogger logger,
-            ISupplier supplier1, ISupplier supplier2, ISupplier supplier3)
+		public ShopService(IDatabaseDriver databaseDriver, ILogger logger, ISupplier[] suppliers)
 		{
-			DatabaseDriver = databaseDriver;
+			this.databaseDriver = databaseDriver;
 			this.logger = logger;
-			Supplier1 = supplier1;
-			Supplier2 = supplier2;
-			Supplier3 = supplier3;
+            this.suppliers = suppliers;
 		}
 
         public static ShopService CreateService()
         {
-            return new ShopService(new DatabaseDriver(), new Logger(),
-                new Supplier1(), new Supplier2(), new Supplier3());
+            ISupplier[] suppliers = new ISupplier[]{
+                new Supplier1(),
+                new Supplier2(),
+                new Supplier3()
+            };
+            return new ShopService(new DatabaseDriver(), new Logger(), suppliers);
+        }
+
+        public Article OrderArticle(int id, int maxExpectedPrice)
+        {
+            Article tempArticle = null;
+
+            foreach (ISupplier supplier in suppliers)
+            {
+                bool articleExists = supplier.ArticleInInventory(id);
+                if (!articleExists)
+                    break;
+                tempArticle = supplier.GetArticle(id);
+                if (maxExpectedPrice >= tempArticle.ArticlePrice)
+                    break;
+            }
+
+            if (tempArticle == null)
+            {
+                throw new Exception("Could not order article");
+            }
+
+            return tempArticle;
+        }
+
+        public void SellArticle(int id, Article article, int buyerId)
+        {
+            logger.Debug("Trying to sell article with id=" + id);
+
+            article.IsSold = true;
+            article.SoldDate = DateTime.Now;
+            article.BuyerUserId = buyerId;
+
+            try
+            {
+                databaseDriver.Save(article);
+                logger.Info("Article with id=" + id + " is sold.");
+            }
+            catch (ArgumentNullException ex)
+            {
+                logger.Error("Could not save article with id=" + id);
+                throw new Exception("Could not save article with id");
+            }
+            catch (Exception)
+            {
+            }
         }
 
 		public void OrderAndSellArticle(int id, int maxExpectedPrice, int buyerId)
 		{
-			#region ordering article
-
-			Article article = null;
-			Article tempArticle = null;
-			var articleExists = Supplier1.ArticleInInventory(id);
-			if (articleExists)
-			{
-				tempArticle = Supplier1.GetArticle(id);
-				if (maxExpectedPrice < tempArticle.ArticlePrice)
-				{
-					articleExists = Supplier2.ArticleInInventory(id);
-					if (articleExists)
-					{
-						tempArticle = Supplier2.GetArticle(id);
-						if (maxExpectedPrice < tempArticle.ArticlePrice)
-						{
-							articleExists = Supplier3.ArticleInInventory(id);
-							if (articleExists)
-							{
-								tempArticle = Supplier3.GetArticle(id);
-								if (maxExpectedPrice < tempArticle.ArticlePrice)
-								{
-									article = tempArticle;
-								}
-							}
-						}
-					}
-				}
-			}
-			
-			article = tempArticle;
-			#endregion
-
-			#region selling article
-
-			if (article == null)
-			{
-				throw new Exception("Could not order article");
-			}
-
-			logger.Debug("Trying to sell article with id=" + id);
-
-			article.IsSold = true;
-			article.SoldDate = DateTime.Now;
-			article.BuyerUserId = buyerId;
-			
-			try
-			{
-				DatabaseDriver.Save(article);
-				logger.Info("Article with id=" + id + " is sold.");
-			}
-			catch (ArgumentNullException ex)
-			{
-				logger.Error("Could not save article with id=" + id);
-				throw new Exception("Could not save article with id");
-			}
-			catch (Exception)
-			{
-			}
-
-			#endregion
+            Article article = OrderArticle(id, maxExpectedPrice);
+            SellArticle(id, article, buyerId);
 		}
 
 		public Article GetById(int id)
 		{
-			return DatabaseDriver.GetById(id);
+			return databaseDriver.GetById(id);
 		}
 	}
 }
